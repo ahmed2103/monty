352999fee6da12e078d55a55db5614c47ad779f3
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-17 14:54:04 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,26 +1,46 @@
 #include "monty.h"
 
+char stack_queue = 's';
+
 /**
- * main - Entry point of the program
+ * main - Entry point of the Monty interpreter
  * @argc: Number of arguments passed to the program
  * @argv: Array of strings containing the arguments
+ *
  * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+    FILE *stream = NULL;
+    size_t len = 0;
+    unsigned int line_number = 1;
+    char *line = NULL;
+    char *code = NULL;
+    stack_t *stack = NULL;
+
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return EXIT_FAILURE;
+        dprintf(STDERR_FILENO, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
     }
 
-    if (!open_file(argv[1]))
+    stream = fopen(argv[1], "r");
+    if (stream == NULL)
     {
-        fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
-        return EXIT_FAILURE;
+        dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
     }
 
-    free_nodes();
+    while (getline(&line, &len, stream) != -1)
+    {
+        code = strtok(line, " \t\r\n\v\f");
+        if (code != NULL && code[0] != '#')
+            execute_opcode(&stack, line_number, code);
+        line_number++;
+    }
+
+    free_stack(stack);
+    free(line);
+    fclose(stream);
     return EXIT_SUCCESS;
 }
