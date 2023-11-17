977ced527d62a600564f82a5c9da0c38118c50e8
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-17 18:16:38 +0200

    Update comp.c

--- comp.c
+++ comp.c
@@ -14,7 +14,7 @@ void pop(stack_t **stack, unsigned int line_number)
     if (*stack == NULL)
     {
         fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        free_stack_t(*stack);
+        free_stack(*stack);
         exit(EXIT_FAILURE);
     }
 
