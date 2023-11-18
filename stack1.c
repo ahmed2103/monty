2b5b93b6f21809397166219fea2496c62647211f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-18 08:01:37 +0200

    Update stack1.c

--- stack1.c
+++ stack1.c
@@ -11,7 +11,7 @@ void pint(stack_t **stack, unsigned int line_number)
     if (*stack == NULL)
     {
         dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
-        free_stack_t(*stack);
+        free_stack(*stack);
         exit(EXIT_FAILURE);
     }
 
