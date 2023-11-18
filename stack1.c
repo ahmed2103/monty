4f21c17cd894047fae376eca747f3815216beb6f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-18 08:00:58 +0200

    Update stack1.c

--- stack1.c
+++ stack1.c
@@ -1,4 +1,22 @@
 #include "monty.h"
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: double pointer to header (top) of the stack.
+ * @line_number: counter for line number of the file.
+ *
+ * Return: void.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+        free_stack_t(*stack);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*stack)->n);
+}
 
 /**
  * stack - Selects stack mode (default).
